93640c11719fc71127555bd1eded23142cbaece6
pralhadadhikari <101581753+pralhadadhikari@users.noreply.github.com>
2024-05-24 19:22:51 +0545

    stock handle in rateinfotable

--- IMS/IMS.web/obj/Debug/net6.0/IMS.web.AssemblyInfo.cs
+++ IMS/IMS.web/obj/Debug/net6.0/IMS.web.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IMS.web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a14708dd8c4172232740aa2d7cef99565bb2b71")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12862ff13288082eb3274e96390f3d92c4b3488f")]
 [assembly: System.Reflection.AssemblyProductAttribute("IMS.web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IMS.web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
